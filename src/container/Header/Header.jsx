57df41edcabfe438da8c7ed94fddb81d43ce5a2a
Fuad Maulana <fuadm03@gmail.com>
2023-08-29 01:16:13 +0700

    implement header component and styling

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,19 @@
+import {SubHeading} from '../../components';
 import './Header.css';
+import {images} from '../../constants';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volupat morbi facilis quam scelerisque sapien. Et, penatibus amet tellus.</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header image" />
+    </div>
   </div>
 );
 
