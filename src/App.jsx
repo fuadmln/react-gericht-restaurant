8408926afda834c7df8601001170c4033b94714e
Fuad Maulana <fuadm03@gmail.com>
2023-08-28 23:18:39 +0700

    refactor: arrow function to regular function

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, Special
 import { Navbar } from './components';
 import './App.css';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
-);
+function App(){
+  return(
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
+  
+}
 
 export default App;
