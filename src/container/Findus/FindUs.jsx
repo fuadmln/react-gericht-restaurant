d543e7a55cb0de466e49d91f22c51f5d21626de7
Fuad Maulana <fuadm03@gmail.com>
2023-08-31 17:37:25 +0700

    implement find us component and styling

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,6 +1,23 @@
+import {SubHeading} from '../../components';
+import {images} from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__warpper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
+        <p className="p__cormorant">Mon - Fri: 10.00am - 02.00am</p>
+        <p className="p__cormorant">Sat - Sun: 10.00am - 03.00am</p>
+      </div>
+      <button className="custom__button" style={{ marginTop: '2rem' }}>Visit Us</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="find us" />
+    </div>
   </div>
 );
 
